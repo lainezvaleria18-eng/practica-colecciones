a9dce0928513801c9ed8632bc9f6007993dbcc4d
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-06-05 07:57:27 -0600

    Karla Yamileth Ceren Perez - Ejercicio 2 pag.70 finalizado.

---
+++ src/Bloque3/Alumno.java
@@ -0,0 +1,37 @@
+package Bloque3;
+public class Alumno {
+
+    private String codigo;
+    private String nombre;
+    private String carnet;
+
+    public Alumno(String codigo, String nombre, String carnet) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.carnet = carnet;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+}
---
+++ src/Bloque3/Ejercicio2ArrayList.java
@@ -0,0 +1,23 @@
+package Bloque3;
+import java.util.ArrayList;
+
+public class Ejercicio2ArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+
+        listaAlumnos.add(new Alumno("A001", "Karla Perez", "CP23001"));
+        listaAlumnos.add(new Alumno("A002", "Juan Lopez", "CP23002"));
+        listaAlumnos.add(new Alumno("A003", "Maria Gomez", "CP23003"));
+
+        System.out.println("LISTA DE ALUMNOS");
+
+        for (Alumno alumno : listaAlumnos) {
+            System.out.println(
+                    alumno.getCodigo() + " - "
+                            + alumno.getNombre() + " - "
+                            + alumno.getCarnet());
+        }
+    }
+}
